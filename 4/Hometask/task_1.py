137f87287f0b462b1c6b6b41d19699d0da673d8a
stivo32 <25997870>
2017-04-16 12:56:47 +0300

    4 lesson, task_2

---
+++ 4/Hometask/task_1.py
@@ -0,0 +1,15 @@
+# coding: utf-8
+
+for i in range(0, 11):
+    for j in range(0, 11):
+        if i == 0 and j == 0:
+            print ' '.rjust(4),
+            continue
+        elif i == 0:
+            i = 1
+        elif j == 0:
+            j = 1
+
+        string_to_print = '{}'.format(j * i)
+        print string_to_print.rjust(4),
+    print
