d4e2a092c8c405b82690f7d4ea0e8b4fdd7b00aa
stivo32 <stivo32@gmail.com>
2017-06-15 21:00:06 +0300

    OOP задачи

--- 7/Hometask/fraction.py
+++ 7/Hometask/fraction.py
@@ -10,54 +10,71 @@ class Fraction(object):
 		if self.numerator % self.denominator == 0:
 			result_string = '{}({})'.format(Fraction.__name__, self.numerator / self.denominator)
 		else:
-
+			self._normal_form()
 			result_string = '{}({}, {})'.format(Fraction.__name__, self.numerator, self.denominator)
 		return result_string
 
 	def __add__(self, other):
+		temp_numerator = 1
+		temp_denominator = 1
 		if isinstance(other, int):
-			self.numerator += other * self.denominator
+			temp_numerator = self.numerator + other * self.denominator
 		elif isinstance(other, Fraction):
 			if self.denominator == other.denominator:
-				self.numerator += other.numerator
+				temp_numerator = self.numerator + other.numerator
+				temp_denominator = self.denominator
 			else:
-				self.numerator = self.numerator * other.denominator + other.numerator * self.denominator
-				self.denominator *= other.denominator
-		return Fraction(self.numerator, self.denominator)
+				temp_numerator = self.numerator * other.denominator + other.numerator * self.denominator
+				temp_denominator = self.denominator * other.denominator
+		return Fraction(temp_numerator, temp_denominator)
 
 	def __mul__(self, other):
+		temp_numerator = 1
+		temp_denominator = 1
 		if isinstance(other, int):
-			self.numerator *= other
+			temp_numerator = self.numerator * other
 		elif isinstance(other, Fraction):
-			self.numerator *= other.numerator
-			self.denominator *= other.denominator
-		return Fraction(self.numerator, self.denominator)
+			temp_numerator = self.numerator * other.numerator
+			temp_denominator = self.denominator * other.denominator
+		return Fraction(temp_numerator, temp_denominator)
 
 	def __div__(self, other):
+		temp_numerator = 1
+		temp_denominator = 1
 		if isinstance(other, int):
-			self.denominator *= other
+			temp_denominator = self.denominator * other
 		elif isinstance(other, Fraction):
-			self.numerator *= other.denominator
-			self.denominator *= other.numerator
-		return Fraction(self.numerator, self.denominator)
+			temp_numerator = self.numerator * other.denominator
+			temp_denominator = other.numerator * self.denominator
+
+		return Fraction(temp_numerator, temp_denominator)
 
 	def __sub__(self, other):
+		temp_numerator = 1
+		temp_denominator = 1
 		if isinstance(other, int):
-			self.numerator -= other * self.denominator
+			temp_numerator = self.numerator - other * self.denominator
+			temp_denominator = self.denominator
 		elif isinstance(other, Fraction):
 			if self.denominator == other.denominator:
-				self.numerator -= other.numerator
+				temp_numerator = self.numerator - other.numerator
+				temp_denominator = self.denominator
 			else:
-				self.numerator = self.numerator * other.denominator - other.numerator * self.denominator
-				self.denominator *= other.denominator
-		return Fraction(self.numerator, self.denominator)
+				temp_numerator = self.numerator * other.denominator - other.numerator * self.denominator
+				temp_denominator = self.denominator * other.denominator
+		return Fraction(temp_numerator, temp_denominator)
 
 	def _normal_form(self):
 		min_number = min(self.numerator, self.denominator)
 		simple_list = simple_numbers(min_number)
-		for number in xrange(simple_list):
-			pass
-		return simple_list
+		for number in simple_list:
+			while True:
+				if self.numerator % number == 0 and self.denominator % number == 0:
+					self.numerator /= number
+					self.denominator /= number
+					# print '{} / {}'.format(self.numerator, self.denominator)
+				else:
+					break
 
 
 def simple_numbers(number):
@@ -84,12 +101,10 @@ def simple_numbers(number):
 
 
 def main():
-	frac = Fraction(33, 11)
-	# frac2 = Fraction(4, 6)
-	# print (frac - 3) * 3
-	# #print frac + frac2
-	print frac._normal_form()
-	#print simple_numbers(22)
+	frac = Fraction(32, 1024)
+	frac2 = Fraction(256, 16)
+	print frac * frac2
+
 
 if __name__ == '__main__':
 	main()
